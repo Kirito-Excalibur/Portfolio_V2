a49e1df849b81bee640a0296ca40ec0c18d7c206
Nikhil <nikhilsimontoppo@gmail.com>
2023-12-03 08:32:56 +0530

    Beginning 3d model

---
+++ pages/posts.js
@@ -0,0 +1,15 @@
+"use client";
+import {Heading, Container} from "@chakra-ui/react";
+import Layout from "../components/layouts/article";
+
+const Works = () => (
+    <Layout>
+        <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+        Posts
+        </Heading>
+        </Container>
+    </Layout>
+)
+
+export default Works
--- pages/works.js
+++ pages/works.js
@@ -2,68 +2,94 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItems } from '../components/grid-item'
-import thumbInkrop from '../public/images/works/inkdrop_01.png'
 import Layout from '../components/layouts/article'
 const Works = () => {
   return (
     <Layout>
-    <Container>
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
-          <WorkGridItems
-            src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
-            title="LEAD 4.0"
-            thumbnail="https://mir-s3-cdn-cf.behance.net/project_modules/1400/a6a27d183305525.653d40764de8c.png"
-          >
-            Life is wierd. So is Sakshi
-          </WorkGridItems>
-        </Section>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItems
+              src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
+              title="LEAD 4.0"
+              thumbnail="/images/works/lead_4.0.png"
+            >
+              Life is wierd. So is Sakshi
+            </WorkGridItems>
+          </Section>
 
-        <Section>
-          <WorkGridItems
-            src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
-            title="Finance Club"
-            thumbnail="https://mir-s3-cdn-cf.behance.net/projects/max_808/58d78f171117395.Y3JvcCwxMDgwLDg0NCwwLDI1MQ.jpeg"
-          >
-            Life is wierd. So is Sakshi
-          </WorkGridItems>
-        </Section>
+          <Section>
+            <WorkGridItems
+              src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
+              title="Finance Club"
+              thumbnail="/images/works/finance.png"
+            >
+              Life is wierd. So is Sakshi
+            </WorkGridItems>
+          </Section>
 
-        <Section>
-          <WorkGridItems
-            src="https://www.behance.net/gallery/171117447/SupportLocalBusiness"
-            title="Support Local Businesses"
-            thumbnail="https://mir-s3-cdn-cf.behance.net/projects/max_808/87a3f9171117447.Y3JvcCw4OTksNzAzLDAsNDQ3.jpeg"
-          >
-            Life is wierd. So is Sakshi
-          </WorkGridItems>
-        </Section>
+          <Section>
+            <WorkGridItems
+              src="https://www.behance.net/gallery/171117447/SupportLocalBusiness"
+              title="Support Local Businesses"
+              thumbnail="/images/works/dark_knight.png"
+            >
+              Life is wierd. So is Sakshi
+            </WorkGridItems>
+          </Section>
 
-        <Section>
-          <WorkGridItems
-            src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
-            title="LEAD 4.0"
-            thumbnail="https://mir-s3-cdn-cf.behance.net/project_modules/1400/a6a27d183305525.653d40764de8c.png"
-          >
-            Life is wierd. So is Sakshi
-          </WorkGridItems>
-        </Section>
+          <Section>
+            <WorkGridItems
+              src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
+              title="Blue Future"
+              thumbnail="/images/works/blue_future.png"
+            >
+              Life is wierd. So is Sakshi
+            </WorkGridItems>
+          </Section>
+          <Section>
+            <WorkGridItems
+              src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
+              title="Rakhsha Bandhan"
+              thumbnail="/images/works/Rakhi.png"
+            >
+              Life is wierd. So is Sakshi
+            </WorkGridItems>
+          </Section>
 
-        <Section>
-          <WorkGridItems
-            src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
-            title="LEAD 4.0"
-            thumbnail="https://mir-s3-cdn-cf.behance.net/project_modules/1400/a6a27d183305525.653d40764de8c.png"
-          >
-            Life is wierd. So is Sakshi
-          </WorkGridItems>
-        </Section>
-      </SimpleGrid>
-    </Container>
+          <Section>
+            <WorkGridItems
+              src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
+              title="Night Club Party"
+              thumbnail="/images/works/night__club_party.png"
+            >
+              Life is wierd. So is Sakshi
+            </WorkGridItems>
+          </Section>
+          <Section>
+            <WorkGridItems
+              src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
+              title="Govardhan"
+              thumbnail="/images/works/govardhan.png"
+            >
+              Life is wierd. So is Sakshi
+            </WorkGridItems>
+          </Section>
+          <Section>
+            <WorkGridItems
+              src="https://www.behance.net/gallery/183305525/IEEE-LEAD-40"
+              title="Music Festival"
+              thumbnail="/images/works/music_festival.png"
+            >
+              Life is wierd. So is Sakshi
+            </WorkGridItems>
+          </Section>
+        </SimpleGrid>
+      </Container>
     </Layout>
   )
 }
